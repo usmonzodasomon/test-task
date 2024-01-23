c116f9846227b4549a432e8cd9e5ece8ef5e2646
usmonzodasomon <budushiyprogrammist@gmail.com>
2024-01-23 14:24:25 +0500

    Add server implementation

---
+++ server.go
@@ -0,0 +1,28 @@
+package test_task
+
+import (
+	"context"
+	"net/http"
+	"time"
+)
+
+type Server struct {
+	httpServer *http.Server
+}
+
+func (s *Server) Run(port string, handler http.Handler) error {
+	s.httpServer = &http.Server{
+		Addr:           ":" + port,
+		MaxHeaderBytes: 32 << 20,
+		Handler:        handler,
+		ReadTimeout:    20 * time.Second,
+		WriteTimeout:   20 * time.Second,
+		IdleTimeout:    120 * time.Second,
+	}
+	return s.httpServer.ListenAndServe()
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	return s.httpServer.Shutdown(ctx)
+
+}
