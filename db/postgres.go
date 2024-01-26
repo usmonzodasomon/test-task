6948970b1189bd8ebfd0e56fbca15db689a77e1f
usmonzodasomon <budushiyprogrammist@gmail.com>
2024-01-26 17:42:19 +0500

    Fix environment variable name in postgres.go

--- db/postgres.go
+++ db/postgres.go
@@ -10,9 +10,8 @@ import (
 
 func InitDB() *gorm.DB {
 	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
-		os.Getenv("HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"),
+		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_SSLMODE"))
-
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
